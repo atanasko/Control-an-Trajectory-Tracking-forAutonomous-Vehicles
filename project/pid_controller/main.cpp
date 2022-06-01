0461c603e1f237befa924fa7c29b9c997bff5139
Atanasko Boris Mitrev <atanasko.mitrev@gmail.com>
2022-06-02 01:16:11 +0200

    Update PID controller parameters

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -227,7 +227,7 @@ int main ()
     * TODO (Step 1): create pid (pid_throttle) for throttle command and initialize values
     **/
     PID pid_throttle = PID();
-    pid_throttle.Init(0.2,0.001,0.019,1.0,-1.0);
+    pid_throttle.Init(0.2,0.001,0.1,1.0,-1.0);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
