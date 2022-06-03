3db12f109bac75de680aa626390678d0fb37ccb0
Atanasko Boris Mitrev <atanasko.mitrev@gmail.com>
2022-06-03 23:23:32 +0200

    Update PID controller parameters

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -233,7 +233,7 @@ int main ()
   * TODO (Step 1): create pid (pid_steer) for steer command and initialize values
   **/
     PID pid_steer = PID();
-    pid_steer.Init(0.29,0.0011,0.7,1.2,-1.2);
+    pid_steer.Init(0.3,0.001,0.3,1.2,-1.2);
 
 
     // initialize pid throttle
@@ -241,7 +241,7 @@ int main ()
     * TODO (Step 1): create pid (pid_throttle) for throttle command and initialize values
     **/
     PID pid_throttle = PID();
-    pid_throttle.Init(0.2,0.001,0.3,1.0,-1.0);
+    pid_throttle.Init(0.2,0.0009,0.1,1.0,-1.0);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
